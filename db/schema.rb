68fb7b6a33d54f2b7a1c43e69c0896497dedc9a3
“rpr_maddi” <ram.prasad@introhive.com>
2019-10-20 19:11:16 +0530

    Folder related model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_160654) do
+ActiveRecord::Schema.define(version: 2019_10_20_134035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "folders", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_folders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_10_19_160654) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "folders", "users"
 end
