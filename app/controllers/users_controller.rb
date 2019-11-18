a39a7a70186bc0b837e1ccbae328e8d9511315eb
“rpr_maddi” <ram.prasad@introhive.com>
2019-11-18 21:26:34 +0530

    Internationalized static strings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     end
 
   rescue
-    render json: { error: { aws: 'Unable to create a folder for user in aws at the moment. Please try after sometime.' } }, status: :interal_server_error
+    render json: { error: { aws: I18n.t('user.signup_folder_create_error') } }, status: :interal_server_error
   end
 
   def sign_in
@@ -22,9 +22,9 @@ class UsersController < ApplicationController
     user = User.find_by_email(email)
 
     if(!user)
-      render json: { errors: { email: 'No user found with the given email.' } }, status: :bad_request
+      render json: { errors: { email: I18n.t('user.no_user_with_email') } }, status: :bad_request
     elsif user && !user.authenticate(password)
-      render json: { errors: { password: 'Wrong password. Please try again.' } }, status: :bad_request
+      render json: { errors: { password: I18n.t('user.invalid_password') } }, status: :bad_request
     else
       api_token = JsonWebToken.encode({ id: user.id }, browser: request.env['HTTP_USER_AGENT'])
       # render json: UserSerializer.new(user), status: :ok
